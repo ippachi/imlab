c212ce16229c05490aed8ec4a4d1fae523970c71
ippachi <ivalice1218@gmail.com>
2017-12-05 23:26:45 +0900

    検出窓描画を5フレームごとに変更

--- human_detect/src/cascade.py
+++ human_detect/src/cascade.py
@@ -11,9 +11,12 @@ for frame in cap.nextFrame():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     point = Cascade.detectMultiScale(gray, 1.06, 4, 0, (140, 140), (160, 160))
 
+    i = 0
     if len(point) > 0:
       for rect in point:
-        cv2.rectangle(gray, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0,255), thickness=2)
+        i += 1
+        if i % 5 == 0:
+          cv2.rectangle(gray, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0,255), thickness=2)
 
     cv2.imshow('frame',gray)
 
