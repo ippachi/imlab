61f4d1e1c68da531729fe237e3de51f928a7fabb
ippachi <ivalice1218@gmail.com>
2017-12-06 14:15:14 +0900

    LBP学習結果

--- human_detect/src/cascade.py
+++ human_detect/src/cascade.py
@@ -6,17 +6,16 @@ cap = sk.FFmpegReader('../input/video/02.avi')
 
 Cascade = cv2.CascadeClassifier('../data/model/cascade.xml')
 
+i = 0
 for frame in cap.nextFrame():
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     point = Cascade.detectMultiScale(gray, 1.06, 4, 0, (140, 140), (160, 160))
 
-    i = 0
-    if len(point) > 0:
+    if len(point) > 0 and i % 2 == 0:
       for rect in point:
-        i += 1
-        if i % 5 == 0:
-          cv2.rectangle(gray, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0,255), thickness=2)
+        cv2.rectangle(gray, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0,255), thickness=2)
+    i += 1
 
     cv2.imshow('frame',gray)
 
